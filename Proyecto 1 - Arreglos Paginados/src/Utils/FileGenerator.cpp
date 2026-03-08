04fdeef5fb7cd50daffbd328e91bacfb866cf877
Marcelo-Matey-R <queridozapato@gmail.com>
2026-03-08 16:51:22 -0600

    Cambios en el buffer principal, mejoramiento del CopyBinaryFile y creacion del metodo CopyTxtFile
    Cambios en el SetFileName para asegurar la extension, creacion de los get de FileName y FileSize

--- Proyecto 1 - Arreglos Paginados/src/Utils/FileGenerator.cpp
+++ Proyecto 1 - Arreglos Paginados/src/Utils/FileGenerator.cpp
@@ -1,16 +1,22 @@
 #include "FileGenerator.h"
+#include <iostream>
 #include <fstream>
 #include <cstdint>
 #include <chrono>
 #include <string>
 #include <string_view>
-
+#include <filesystem>
 
 FileGenerator::FileGenerator(size_t fileSize, std::string_view fileName){
     SetFileName(fileName);
     SetFileSize(fileSize);
 }
 
+void FileGenerator::SetFileName(std::string_view name){
+	fileName = name;
+	std::filesystem::path file(fileName);
+	file.replace_extension(".bin");
+}
 
 void FileGenerator::GenerateFile(){
     std::ofstream file; //Crear la variable del file
@@ -30,7 +36,7 @@ void FileGenerator::GenerateFile(){
 			buff[i+3] = gen();
 		}
 
-		file.write(reinterpret_cast<const char*>(buff.get()), totalBytes);
+		file.write(reinterpret_cast<const char*>(&buff[0]), totalBytes);
 
 		if(file.fail()){
 			std::cout<<"Error de escritura"<<"\n";
@@ -45,15 +51,17 @@ void FileGenerator::GenerateFile(){
 }
 
 
-bool FileGenerator::CopyFile(std::string name){
-    std::ifstream origin;
-    std::ofstream destiny;
+bool FileGenerator::CopyBinaryFile(std::string name){
+	std::ifstream origin;
+	std::ofstream destiny;
 
-    origin.open(fileName, std::ios::in | std::ios::binary);
-    destiny.open(name, std::ios::out | std::ios::binary);
+	origin.open(fileName, std::ios::in | std::ios::binary);
+	destiny.open(name, std::ios::out | std::ios::binary);
 
     if(!origin.is_open() || !destiny.is_open()){
         std::cout<<"Error al abrir alguno de los archivos"<<"\n";
+		origin.close();
+		destiny.close();
         return false;
     }
 
@@ -61,8 +69,80 @@ bool FileGenerator::CopyFile(std::string name){
 
     if(destiny.fail()){
         std::cout<<"Error de escritura"<<"\n";
+		origin.close();
+		destiny.close();
         return false;
     }
+	origin.close();
+	destiny.close();
     return true;
 
+}
+
+bool FileGenerator::CopyTxtFile(std::string name){
+	std::ifstream origin;
+	std::ofstream destiny;
+
+	//asegura la extension de la ruta
+	std::filesystem::path f(name);
+	f.replace_extension(".txt");
+
+	origin.open(fileName, std::ios::in | std::ios::binary);
+	destiny.open(name, std::ios::out);
+
+	//almacenamiento de numeros
+	std::string str; 
+	str.reserve(11 * totalBytes);
+
+    if(!origin.is_open() || !destiny.is_open()){
+        std::cout<<"Error al abrir alguno de los archivos"<<"\n";
+		origin.close();
+		destiny.close();
+        return false;
+    }
+
+	auto start = std::chrono::steady_clock::now();
+	while(true){
+		str.clear();
+
+
+		origin.read(reinterpret_cast<char*>(&buff[0]), totalNumbers);
+		
+		size_t numberByte = origin.gcount(); //cantidad de bytes leidos por iteracion
+		
+		if(numberByte == 0) break; //si no hay bytes entonces se termino la lectura
+
+		if(origin.bad()){
+			std::cout<<"No se pudo leer bien"<<"\n";
+			origin.close();
+			destiny.close();
+			return false;
+		}
+
+		size_t numElements = numberByte/sizeof(int32_t); //Cantidad de numeros
+		
+		for(size_t i = 0; i < numElements; i++){
+			
+			//pasar de numeros a texto y almacernar los numeros en el string
+			str.append(std::to_string(buff[i])); 
+			str += ' ';
+		}
+
+		destiny.write(str.data(), str.size());
+		
+		if(destiny.fail()){
+			std::cout<<"Error de escritura"<<"\n";
+			origin.close();
+			destiny.close();
+			return false;
+    	}	
+		
+	}
+	auto end = std::chrono::steady_clock::now();
+	std::chrono::duration<double> dur = end-start;
+	std::cout<<"tiempo: "<<dur.count()<<"\n";
+	origin.close();
+	destiny.close();
+
+	return true;
 }
--- Proyecto 1 - Arreglos Paginados/src/Utils/FileGenerator.h
+++ Proyecto 1 - Arreglos Paginados/src/Utils/FileGenerator.h
@@ -1,17 +1,14 @@
-#include <iostream>
 #include <cstdint>
 #include <string>
 #include <string_view>
 #include <random>
-#include <memory>
 #include <ostream>
-
 
 #ifndef FILEGENERATOR_H
 #define FILEGENERATOR_H
 class FileGenerator{
     //Calculo de Bytes por array
-    static constexpr size_t arraySize = 1048576;
+    static constexpr size_t arraySize = 32768;
     static constexpr size_t intSize = sizeof(int32_t);
     static constexpr size_t totalBytes = intSize*arraySize;
     
@@ -22,19 +19,22 @@ class FileGenerator{
 
 
     size_t fileSize = 536870912;
-    size_t totalNumbers = fileSize/totalBytes; //Maximo numero de llenados al array
+    size_t totalNumbers = fileSize/totalBytes; //Maximo numero de repeticiones
 
-    std::unique_ptr<int32_t[]> buff = std::make_unique<int32_t[]>(arraySize);
+    int32_t buff[arraySize];
 
     std::string fileName = "";
 
     public:
     FileGenerator(size_t fileSize, std::string_view fileName);
-    void SetFileSize(size_t size){fileSize = size; totalNumbers = fileSize/totalBytes;}
-    void SetFileName(std::string_view name){fileName = name; fileName += ".bin";}
+    void SetFileSize(size_t size){fileSize = (size == 512) ? size*1048576 : size*1073741824; totalNumbers = fileSize/totalBytes;}
+    size_t GetFileSize(){return fileSize;}
+    void SetFileName(std::string_view name);
+    std::string GetFileName(){return fileName;}
 
     void GenerateFile();
-    bool CopyFile(std::string name);
+    bool CopyBinaryFile(std::string name);
+    bool CopyTxtFile(std::string name);
 
 
 };

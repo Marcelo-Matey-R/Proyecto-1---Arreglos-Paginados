0d37440fc765835345d42179507aaa879fd567b4
Marcelo-Matey-R <queridozapato@gmail.com>
2026-03-10 23:31:01 -0600

    creacion del main y el archivo de ejecucion

---
+++ Proyecto 1 - Arreglos Paginados/src/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main(int argc, char* argv[]){
+    if(argc < 6 || argc > 11){
+        std::cout<<"cantidad de comandos no permitida"<<"\n";
+        return 1;
+    }
+
+    
+}

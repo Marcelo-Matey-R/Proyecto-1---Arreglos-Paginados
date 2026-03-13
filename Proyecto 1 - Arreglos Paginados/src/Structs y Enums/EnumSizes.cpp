b2ec9502de58e87a517aa4b95d44d47e9b89fb8a
Marcelo-Matey-R <queridozapato@gmail.com>
2026-03-12 22:57:19 -0600

    Mas opciones de escritura del size

--- Proyecto 1 - Arreglos Paginados/src/Structs y Enums/EnumSizes.cpp
+++ Proyecto 1 - Arreglos Paginados/src/Structs y Enums/EnumSizes.cpp
@@ -4,9 +4,9 @@
 #include <string_view>
 
 SIZES StringToSize(std::string_view str) {
-    if (str == "SMALL")  return SIZES::SMALL;
-    if (str == "MEDIUM") return SIZES::MEDIUM;
-    if (str == "LARGE")  return SIZES::LARGE;
+    if (str == "SMALL" || str == "small" || str == "Small")  return SIZES::SMALL;
+    if (str == "MEDIUM" || str == "medium" || str == "Medium") return SIZES::MEDIUM;
+    if (str == "LARGE" || str == "large" || str == "Large")  return SIZES::LARGE;
     
     // Manejo de error si el string no coincide
     throw std::invalid_argument("Tamaño no válido");

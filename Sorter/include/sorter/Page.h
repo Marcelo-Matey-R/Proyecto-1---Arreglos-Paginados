e5fa8145a6dc4a1e2f02d8ac156625020a3e67de
Marcelo-Matey-R <queridozapato@gmail.com>
2026-03-24 23:38:32 -0600

    Se termina de crear la pagina, para tener un uso de LRU

--- Sorter/include/sorter/Page.h
+++ Sorter/include/sorter/Page.h
@@ -8,6 +8,8 @@ class Page{
     size_t pagedSize;
     size_t inicialPos;
     size_t quantityBytes;
+    bool modified;
+    long long lastUsed;
     public:
     int32_t* page;
     Page(int numberPage, size_t pagedSize);
@@ -15,6 +17,10 @@ class Page{
     size_t GetQuantityBytes(){return quantityBytes;}
     int GetNumberPage(){return numberPage;}
     size_t GetInicialPos(){return inicialPos;}
+    bool GetModified(){return modified;}
+    long long GetLastUsed(){return lastUsed;}
+    void SetModified(bool m){this->modified = m;}
+    void SetLastUsed(long long i){this->lastUsed = i;}
 };
 
 #endif
--- Sorter/src/Page.cpp
+++ Sorter/src/Page.cpp
@@ -1,11 +1,12 @@
 #include "sorter\Page.h"
 
 Page::Page(int numberPaged, size_t pagedSize){
-    this->numberPage = numberPaged;
-    this->pagedSize = pagedSize;
-    this->quantityBytes = pagedSize*sizeof(int32_t);
-    this->inicialPos = numberPage*quantityBytes;
-    this->page = new int32_t[pagedSize];
+    this->numberPage = numberPaged; ///numero de pagina
+    this->pagedSize = pagedSize; //tamanio del archivo
+    this->quantityBytes = pagedSize*sizeof(int32_t); //cantidad bytes a leer
+    this->inicialPos = numberPage*quantityBytes; //posicion inicial en el archivo
+    this->page = new int32_t[pagedSize]{}; //genera el array donde se guardan los numeros, se inicializan en 0
+    this->modified = false;
 }
 Page::~Page(){
     delete[] page;
